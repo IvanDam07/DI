3f436ce51b1789ffec9049fca0742a4476f56530
IvanDam07 <isobrinoc01@educastillalamancha.es>
2024-10-28 12:56:40 +0100

    Actualización 1.1
    - Actualización GameProyect
    - Añadidas nuevas clases e implementación MVC

---
+++ GameProyect/GameProyect/Domain/Ability.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProyect.Domain
+{
+    class Ability
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public int points { get; set; }
+
+        public Ability(String name) { 
+            this.name = name; 
+            this.id = 0;
+            this.points = 100;
+        }
+
+        public Ability(String name, int points)
+        {
+            this.name = name;
+            this.points = points;
+            this.id = 0;
+        }
+    }
+}
---
+++ GameProyect/GameProyect/View/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameProyect
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cboPlayer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //btnNew.IsEnabled = true;
+            //switch (cboPlayer.SelectedIndex)
+            //{
+            //    case 0:
+            //        lblAvaible.Content = "Avaiable Points: " + HT.points;
+            //        break;
+            //    case 1:
+            //        lblAvaible.Content = ""
+            //}
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
