3f3a9f3ba1ca9a0677920d532a63a7b225f16038
IvanDam07 <isobrinoc01@educastillalamancha.es>
2024-10-29 14:39:50 +0100

    Actualización 1.2
    - GameProyect ampliado
    - Mario Bros Grafico empezado

--- GameProyect/GameProyect/Domain/Character.cs
+++ GameProyect/GameProyect/Domain/Character.cs
@@ -3,13 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace GameProyect.Domain
 {
+    /// <summary>
+    /// Character 
+    /// </summary>
     class Character
     {
         public int id { get; set; }
         public string name { get; set; }
         public int points { get; set; }
+
+        int idCounter {  get; set; }
+        /// <summary>
+        /// Constructor for a Character when id, name and points know information
+        /// </summary>
+        /// <param name="id">Unique identificator for the character</param>
+        /// <param name="name">Description name for the character</param>
+        /// <param name="points">Total points for the character, default 0</param>
+        public Character(int id, string name, int points = 0) //Si no recibe el dato de puntos
+        {
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                MessageBox.Show($"\"{nameof(name)}\" Can't be null.", nameof(name));
+                throw new ArgumentException($"\"{nameof(name)}\" Can't be null.", nameof(name));
+            }
+
+            this.id = id;
+            this.name = name;
+            this.points = points;
+            //Update the vaue only when the counter is greater than the current id
+            if (id > idCounter)
+            {
+                idCounter = id;
+            }
+        }
+
+        public Character(string name, int points = 0) //Si no recibe el dato de puntos
+        {
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                MessageBox.Show($"\"{nameof(name)}\" Can't be null.", nameof(name));
+                throw new ArgumentException($"\"{nameof(name)}\" Can't be null.", nameof(name));
+            }
+            this.id = this.idCounter++;
+            this.name = name;
+            this.points = points;
+        }
     }
 }
--- GameProyect/GameProyect/View/MainWindow.xaml.cs
+++ GameProyect/GameProyect/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using GameProyect.Domain;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,8 +20,23 @@ namespace GameProyect
         public MainWindow()
         {
             InitializeComponent();
-        }
 
+            Character character1 = new Character("Fermin", 0);
+            Character character2 = new Character("Pedro", 25);
+            Character character3 = new Character("Leo", 50);
+
+            cboPlayer.Items.Add(character1.name);
+            cboPlayer.Items.Add(character2.name);
+            cboPlayer.Items.Add(character3.name);
+
+            btnNew.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+            btnSave.IsEnabled = false;
+
+            imgWand.IsEnabled = false;
+            imgLightning.IsEnabled = false;
+            imgBrain.IsEnabled = false;
+        }
         private void cboPlayer_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             //btnNew.IsEnabled = true;
@@ -36,7 +52,27 @@ namespace GameProyect
 
         private void btnNew_Click(object sender, RoutedEventArgs e)
         {
+            //// Solicitar el nombre del personaje al usuario
+            //string name = Microsoft.VisualBasic.Interaction.InputBox("Enter the name of the Character:", "New Character");
 
+            //// Verificar que el nombre no esté vacío o nulo
+            //if (string.IsNullOrWhiteSpace(name))
+            //{
+            //    MessageBox.Show("Name cannot be empty.");
+            //    return;
+            //}
+
+            //// Generar un id para el nuevo Character (puedes cambiar la lógica si quieres)
+            //int id = cboPlayer.Items.Count + 1;
+
+            //// Asignar puntos iniciales (puedes cambiar la lógica si quieres)
+            //int points = 0;
+
+            //// Crear el nuevo Character
+            //Character newCharacter = new Character(name, id, points);
+
+            //// Añadir el Character al ComboBox
+            //cboPlayer.Items.Add(newCharacter);
         }
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
