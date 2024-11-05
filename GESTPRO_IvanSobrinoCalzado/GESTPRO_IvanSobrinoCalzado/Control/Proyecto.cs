a92aaafda85215cff3ff06edfc5bec7c5e79f853
IvanDam07 <isobrinoc01@educastillalamancha.es>
2024-11-05 14:40:01 +0100

    Examen

---
+++ GESTPRO_IvanSobrinoCalzado/GESTPRO_IvanSobrinoCalzado/Control/Proyecto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GESTPRO_IvanSobrinoCalzado.Control
+{
+    class Proyecto
+    {
+
+        private String codigoProyecto;
+        private String nombre;
+        private String fechaInicio;
+        private String fechaFin;        
+
+        //Constructor 
+        public Proyecto (String codigoProyecto, String nombre, String fechaInicio, String fechaFin)
+        {
+            this.codigoProyecto = codigoProyecto;
+            this.nombre = nombre;
+            this.fechaInicio = fechaInicio;
+            this.fechaFin = fechaFin;
+        }
+
+        public string CodigoProyecto { get => codigoProyecto; set => codigoProyecto = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string FechaInicio { get => fechaInicio; set => fechaInicio = value; }
+        public string FechaFin { get => fechaFin; set => fechaFin = value; }
+    }
+}
