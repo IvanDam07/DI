62d9eb1765e14efb981e33b46fec36db0d66bb93
IvanDam07 <isobrinoc01@educastillalamancha.es>
2024-11-21 10:43:44 +0100

    Actualización 1.6
    - Añadido ejercicio de excepciones

---
+++ Proyectos/EjemploExcepciones/EjemploExcepciones/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double[] resultados = new double[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+
+            Console.WriteLine($"División {i + 1}");
+
+            try
+            {
+                
+                Console.WriteLine("Ingresa el numerador: ");
+                int numerador = Convert.ToInt32(Console.ReadLine());
+
+                int denominador;
+                Console.WriteLine("Ingresa el denominador: ");
+                denominador = Convert.ToInt32(Console.ReadLine());
+
+                resultados[i] = numerador / denominador;
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("No se puede dividir entre 0. " + e.Message);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("El índice está fuera de los límites del array. " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Programa finalizado");
+            }
+
+        }
+    }
+}
