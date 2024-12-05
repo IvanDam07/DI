1b4c58f4be57ed09dff1fc39f5e26ba113d5636f
IvanDam07 <isobrinoc01@educastillalamancha.es>
2024-12-05 10:32:44 +0100

    TPV actualización 1.0
    Añadidos productos
    Añadidos botones de añadir producto y categoria

--- Proyectos/tpv/tpv/MainWindow.xaml.cs
+++ Proyectos/tpv/tpv/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,60 @@ namespace tpv
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CargarProductosEnStackPanel()
+        {
+            //Conexión a la BBDD
+            string connectionString = "Micadenadeconexion";
+            string query = "SELECT Nombre FROM Productos";
+
+            List<string> productos = new List<string>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                productos.Add(reader.GetString(0)); // Añade cada nombre de producto a la lista
+                            }
+                        }
+                    }
+                }
+
+                //Crear botones dinámicamente
+                foreach(string producto in productos)
+                {
+                    Button boton = new Button
+                    {
+                        Content = producto,
+                        Margin = new Thickness(5),
+                        HorizontalAlignment = HorizontalAlignment.Stretch,
+                        VerticalAlignment = VerticalAlignment.Stretch,
+                    };
+
+                    boton.Click += Boton_Click; //Agrega un evento al click
+                    spCategorias.Children.Add(boton); //Añade botones al StackPanel
+                } 
+
+            } catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar productos: {ex.Message}");
+            }
+        }
+
+        private void Boton_Click(object sender, EventArgs e)
+        {
+            if (sender  is Button boton)
+            {
+                MessageBox.Show($"Hiciste clic en el producto: {boton.Content}"); //Aquí cambiar y que salga una pantalla nueva con los productos
+            }
         }
     }
 }
